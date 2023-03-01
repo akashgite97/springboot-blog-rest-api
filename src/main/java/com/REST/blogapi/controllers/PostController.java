5e0af0f98ae37e1fcdd73669d9a150f6d0dbb8c5
Akash Gite <giteakash97@gmail.com>
2023-03-01 16:40:34 +0530

    post pagination

--- src/main/java/com/REST/blogapi/controllers/PostController.java
+++ src/main/java/com/REST/blogapi/controllers/PostController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.REST.blogapi.payloads.ApiResponse;
@@ -44,9 +45,11 @@ public class PostController {
     }
 
     @GetMapping("/get/all")
-    public ResponseEntity<List<PostDto>> getAllUsers() {
+    public ResponseEntity<List<PostDto>> getAllUsers(
+        @RequestParam(value = "pageNumber",defaultValue = "1", required = false) Integer pageNumber,
+        @RequestParam(value = "pageSize",defaultValue = "5", required = false) Integer pageSize) {
 
-        List<PostDto> users = this.postService.getAllPost();
+        List<PostDto> users = this.postService.getAllPost(pageNumber, pageSize);
 
         return new ResponseEntity<>(users, HttpStatus.CREATED);
     }
--- src/main/java/com/REST/blogapi/services/impl/PostServiceImpl.java
+++ src/main/java/com/REST/blogapi/services/impl/PostServiceImpl.java
@@ -6,6 +6,9 @@ import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.REST.blogapi.entities.Category;
@@ -76,9 +79,13 @@ public class PostServiceImpl implements PostService {
         this.postRepo.delete(post);
     }
 
-     public List<PostDto> getAllPost(){
+     public List<PostDto> getAllPost(Integer pageNumber, Integer pageSize){
 
-        List<Post> posts = this.postRepo.findAll();
+        Pageable  p = PageRequest.of(pageNumber, pageSize);
+        Page<Post> pagePost = this.postRepo.findAll(p);
+
+        List<Post> posts = pagePost.getContent();
+
 
         List<PostDto> postDtos = posts.stream().map(post->this.modelMapper.map(post, PostDto.class)).collect(Collectors.toList());
         
