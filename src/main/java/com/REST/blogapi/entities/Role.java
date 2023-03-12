33b288ebd6e4bff6214767827b121e91afed7073
Akash Gite <giteakash97@gmail.com>
2023-03-12 11:30:42 +0530

    User roles configurations

--- src/main/java/com/REST/blogapi/entities/Role.java
+++ src/main/java/com/REST/blogapi/entities/Role.java
@@ -1,11 +1,21 @@
 package com.REST.blogapi.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
 public class Role {
     
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
     
-    private Sting name;
+    private String name;
 }
--- src/main/java/com/REST/blogapi/repositories/UserRepo.java
+++ src/main/java/com/REST/blogapi/repositories/UserRepo.java
@@ -1,5 +1,7 @@
 package com.REST.blogapi.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
@@ -10,5 +12,6 @@ import com.REST.blogapi.entities.User;
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
 
+    Optional<User> findByEmail(String email);
 
 }
